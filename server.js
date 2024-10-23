e11809a982ae06263ce69aa60692f234a86f234f
adityagaurav95 <adityagaurav995@gmail.com>
2024-10-23 16:36:14 +0530

    Added files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const ruleRoutes = require('./routes/ruleRoutes');
+
+const dotenv = require('dotenv');
+dotenv.config()
+const PORT =  process.env.PORT || 4000 || 5000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.error("Error connecting to MongoDB", err);
+});
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for serving the index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
+app.use('/api/rules', ruleRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
